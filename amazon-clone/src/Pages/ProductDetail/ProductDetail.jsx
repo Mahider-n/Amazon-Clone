28dc16f1e95e2a00d6913e92454dc25856091329
Mahider Tsegaye <Mahidernardos@gmail.com>
2025-02-28 15:12:37 +0300

    loader functionality

--- amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
+++ amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
@@ -6,24 +6,33 @@ import { useParams} from 'react-router-dom';
 import axios from 'axios';
 import { ProductUrl } from '../../Api/endPoints';
 import ProductCard from '../../Components/Product/ProductCard';
+import Loader from '../../Components/Loader/Loader';
 // import Layout from '../../Components/Layout/Layout';
 // import Loader from '../../Components/'
 
 function ProductDetail() {
     const [product,setProduct]=useState({})
+    const[isLoading,setisLoading]=useState(false)
     const {productId}=useParams()
     useEffect(()=>{
+        setisLoading(true)
         axios.get(`${ProductUrl}/products/${productId}`)
         .then((res)=>{
             setProduct(res.data)
+            setisLoading(false)
         }).catch((err)=>{
             console.log("err",err)
+            setisLoading(false)
         })
     },[])
   return (
     <Layout>
-        <ProductCard
-        product={product} />
+        {isLoading? (<Loader/>):(
+            <ProductCard
+            product={product} />
+        )}
+
+        
     </Layout>
   )
 }
--- amazon-clone/src/Pages/Results/Results.jsx
+++ amazon-clone/src/Pages/Results/Results.jsx
@@ -6,17 +6,21 @@ import { ProductUrl } from '../../Api/endPoints'
 import { useEffect } from 'react';
 import classes from '../Results/Results.module.css'
 import ProductCard from '../../Components/Product/ProductCard'
+import Loader from '../../Components/Loader/Loader'
 
 function Results() {
     const [results,setResults]=useState([]);
     const {categoryName} =useParams();
+    const[isLoading,setisLoading]=useState(false)
 
     useEffect(()=>{
         axios.get(`${ProductUrl}/products/category/${categoryName}`)
         .then((res)=>{
             setResults(res.data)
+            setisLoading(false)
         }).catch((err)=>{
             console.log("err",err)
+            setisLoading(false)
         })
     }, [])
 
@@ -26,11 +30,15 @@ function Results() {
             <h1 style={{padding: "30px"}}>Results</h1>
             <p style={{padding: "30px"}}>Catagory /{categoryName}</p>
             <hr />
-            <div className={classes.product_container}>
+            {isLoading ?(
+                <Loader />
+            ):(
+                <div className={classes.product_container}>
                 {results?.map((product)=>(
-                    <ProductCard key={product.id} product={product} />
+                    <ProductCard key={product.id} product={product} renderAdd={true} />
                 ))}
             </div>
+            )}
         </section>
     </Layout>
   )
